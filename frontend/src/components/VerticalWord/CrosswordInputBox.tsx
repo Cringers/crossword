e94f49aa666551d651070ff0ec4ca40f51dcee18
normanjli <norman@808partners.com>
2022-04-11 14:16:54 -0600

    ethan is crossword mockup

---
+++ frontend/src/components/VerticalWord/CrosswordInputBox.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import React from "react";
+import { memo, FormEvent } from "react";
+
+const CrosswordBox = styled.div`
+  background-color: white;
+  border-top: black 1px solid;
+  border-left: black 1px solid;
+  border-right: black 1px solid;
+  width: 2em;
+  height: 2em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:focus {
+    background-color: gray;
+  }
+  caret-color: transparent;
+`;
+
+export type CrossWordInputBoxProps = {
+  onInput?: (event: FormEvent<HTMLDivElement>) => void;
+  onDelete: (event: React.KeyboardEvent<HTMLDivElement>) => void;
+};
+const CrosswordInput = ({ onDelete, onInput }: CrossWordInputBoxProps) => {
+  return (
+    <CrosswordBox contentEditable onInput={onInput} onKeyDown={onDelete} />
+  );
+};
+const CrosswordInputBox = memo(CrosswordInput);
+export default CrosswordInputBox;
--- packages/config/index.ts
+++ packages/config/index.ts
@@ -1,3 +1,3 @@
 export const CONFIG = {
-    PORT: '4000',
-}
+  PORT: "4000",
+};
