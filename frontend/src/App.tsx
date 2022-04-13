3b0f200d92dcbc03cee3ea7cc97757163d8aa38d
Ethan DeSon <ethandeson@gmail.com>
2022-04-12 18:17:48 -0700

    use react hook autogeneration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import CrosswordBoxContainer from './components/VerticalWord/CrosswordBoxContainer';
 import { useQuery } from '@apollo/client'
 import {TestQuery, TestDocument} from './generated/generated';
-import {CrosswordQuery, CrosswordDocument} from './generated/generated';
+import {CrosswordQuery, CrosswordDocument, useCrosswordQuery} from './generated/generated';
 
 function App() {
   const { data } = useQuery<TestQuery>(TestDocument)
-  const grid = useQuery<CrosswordQuery>(CrosswordDocument)
-  
+  const grid = useCrosswordQuery()
+
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>Ethan is?</h1>
