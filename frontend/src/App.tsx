1bf6814a9e8e21f86198392a416b9893be67e00f
Ethan DeSon <ethandeson@gmail.com>
2022-04-12 19:23:57 -0700

    Adds Grid API with mock resolver (#11)
    * add generate command to top level
    
    * Adds grid API; yarn generate now does backend + frontend
    
    * use react hook autogeneration
    
    * integrate with norman's changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-
 import CrosswordBoxContainer from './components/VerticalWord/CrosswordBoxContainer';
+import { useQuery } from '@apollo/client'
+import {TestQuery, TestDocument} from './generated/generated';
+import {CrosswordQuery, CrosswordDocument, useCrosswordQuery} from './generated/generated';
 
 function App() {
+  const { data } = useQuery<TestQuery>(TestDocument)
+  const grid = useCrosswordQuery()
+
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>Ethan is?</h1>
-      <CrosswordBoxContainer value="dralssa" />;
+      <CrosswordBoxContainer value={grid?.data?.crossword.name as string}/>;
     </>
   );
 }
