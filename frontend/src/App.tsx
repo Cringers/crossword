270cac5ae05a0bc15998b2048b4da41e1269ba61
Norman Li <norman@808partners.com>
2022-04-12 19:25:56 -0600

    Feature/UI (#9)
    * ethan is crossword mockup
    
    * added types for styled compoenents
    
    * chore: fix, prettierconfig
    
    * Update README.md (#8)
    
    (cherry picked from commit a411bc9c553a7e654f3f681cc47aa11c5a959772)
    
    Co-authored-by: Josh Ice <JoshIce@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { useQuery } from '@apollo/client'
-import {TestQuery, TestDocument} from './generated/generated';
+
+import CrosswordBoxContainer from './components/VerticalWord/CrosswordBoxContainer';
 
 function App() {
-  const { data } = useQuery<TestQuery>(TestDocument)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Data: {data?.test.name}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{ textAlign: 'center' }}>Ethan is?</h1>
+      <CrosswordBoxContainer value="dralssa" />;
+    </>
   );
 }
 
