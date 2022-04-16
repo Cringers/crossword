056fb8de556dd739d365e322c1cf30a3cd0c865e
Josh Ice <JoshIce@users.noreply.github.com>
2022-04-15 17:27:55 -0700

    Separate staging environments (#25)
    * chore/env variables
    
    * added root route to serve built index.html
    
    * separated server creation logic by stage
    
    * proxy requests to /graphql to localhost:4000/graphql
    
    * Use separate express apps for frontend/backend
    
    Co-authored-by: normanjli <normanjli@gmail.com>
    Co-authored-by: Ethan DeSon <ethandeson@gmail.com>

--- packages/config/index.ts
+++ packages/config/index.ts
@@ -1,3 +1,8 @@
-export const CONFIG = {
-  PORT: "4000",
-};
+const { NODE_ENV } = process.env;
+export const CONFIG = Object.freeze({
+  STAGE: NODE_ENV,
+  FRONTEND_PORT: NODE_ENV === 'development' ? 3000 : 443,
+  BACKEND_PORT: 4000,
+  SSL_CERT: NODE_ENV === 'production' ? '/home/opc/certificate.crt' : '',
+  SSL_KEY: NODE_ENV === 'production' ? '/home/opc/private.key' : '',
+});
