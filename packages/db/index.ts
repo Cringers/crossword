2040ee1e5aea32bae98994ccb2f4489bde0ff9d7
Ethan DeSon <ethandeson@gmail.com>
2022-04-16 19:13:05 -0700

    remove password

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -7,7 +7,7 @@ export const AppDataSource = new DataSource({
     host: "adb.us-phoenix-1.oraclecloud.com",
     port: 1521,
     username: "admin",
-    password: "nhwaOAhsc4a2",
+    password: "",
     serviceName: "g9d823141ae4a7a_crossword_medium.adb.oraclecloud.com",
     extra: {
         connectString: '(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1521)(host=adb.us-phoenix-1.oraclecloud.com))(connect_data=(service_name=g9d823141ae4a7a_crossword_medium.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)(ssl_server_cert_dn="CN=adwc.uscom-east-1.oraclecloud.com, OU=Oracle BMCS US, O=Oracle Corporation, L=Redwood City, ST=California, C=US")))'
