a63bc080d6379ef3e5d33d415493f3389aaf4e75
Josh Ice <josh.j.ice@gmail.com>
2022-04-19 16:19:33 -0700

    added .prettierconfig & formatted remaining files

--- backend/resolvers.ts
+++ backend/resolvers.ts
@@ -1,34 +1,34 @@
 import { AppDataSource } from '@crossword/db';
-import { Crossword as CrosswordEntity} from './entities/crossword';
+import { Crossword as CrosswordEntity } from './entities/crossword';
 import { Resolvers, Crossword, Point } from './generated/graphql';
 
 export const resolvers: Resolvers = {
-  Query: {
-    crossword: async (): Promise<Crossword> => {
-      let crossword : CrosswordEntity = (await AppDataSource.manager.findOneBy(CrosswordEntity,{name: "cringe"}))
-      let name : string = crossword.name
-      let points : Point[] = []
-      crossword.grid.forEach( (row : Array<string>, y)  => {
-        row.forEach((value, x) => {
-          const point : Point = {
-            x,
-            y, 
-            value,
-            first: true,
-            across: true,
-            hint: 1,
-          }
-          points.push(point)
-        })
-      })
-      return {
-        name,
-        id: 'someId',
-        grid: {
-         dimension: crossword.grid.length,
-         points
-        },
-      };
-    },
-  },
+   Query: {
+      crossword: async (): Promise<Crossword> => {
+         const crossword: CrosswordEntity = await AppDataSource.manager.findOneBy(CrosswordEntity, { name: 'cringe' });
+         const { name } = crossword;
+         const points: Point[] = [];
+         crossword.grid.forEach((row: Array<string>, y) => {
+            row.forEach((value, x) => {
+               const point: Point = {
+                  x,
+                  y,
+                  value,
+                  first: true,
+                  across: true,
+                  hint: 1,
+               };
+               points.push(point);
+            });
+         });
+         return {
+            name,
+            id: 'someId',
+            grid: {
+               dimension: crossword.grid.length,
+               points,
+            },
+         };
+      },
+   },
 };
