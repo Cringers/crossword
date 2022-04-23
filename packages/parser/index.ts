8d217cc3b1498c84f1e7f10456fb7dabfb89e1df
Josh Ice <josh.j.ice@gmail.com>
2022-04-23 16:03:26 -0700

    added sqlite3 dev database for local dev

--- packages/parser/index.ts
+++ packages/parser/index.ts
@@ -1,24 +1,23 @@
-import {Crossword} from "@crossword/backend/entities/crossword"
+import { Crossword } from '@crossword/backend/entities/crossword';
 import * as fs from 'fs';
 import * as path from 'path';
 
-
 interface Parser {
-    name: string;
-    (crossword: string): Crossword;
+   name: string;
+   (crossword: string): Crossword;
 }
 
-const crosswordParser : Parser = (crossword: string) => {
-    const crosswordString : string = fs.readFileSync(path.resolve(__dirname, "../../backend/crosswords/"+crossword)).toString()
-    const crosswordEntity : Crossword = new Crossword();
-    const grid = crosswordString.split('\n').map(str => str.split(''));
-    console.log(grid)
-    let first = crosswordString.split('\n');
-    console.log("first",first)
-    crosswordEntity.grid = crosswordString.split('\n').map(str => str.split(''));
-    console.log(crosswordEntity.grid)
-    crosswordEntity.name = crossword
-    return crosswordEntity;
-}
+const crosswordParser: Parser = (crossword: string) => {
+   const crosswordString: string = fs.readFileSync(path.resolve(__dirname, `../../backend/crosswords/${crossword}`)).toString();
+   const crosswordEntity: Crossword = new Crossword();
+   const grid = crosswordString.split('\n').map((str) => str.split(''));
+   console.log(grid);
+   const first = crosswordString.split('\n');
+   console.log('first', first);
+   crosswordEntity.grid = crosswordString.split('\n').map((str) => str.split(''));
+   console.log(crosswordEntity.grid);
+   crosswordEntity.name = crossword;
+   return crosswordEntity;
+};
 
-export { crosswordParser }
+export { crosswordParser };
