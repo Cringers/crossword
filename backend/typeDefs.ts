97a10aea259e83aefd500a7ff828ff355fa084a0
Ethan DeSon <ethandeson@gmail.com>
2022-04-24 09:00:14 -0700

    change grid from string[][] to Point[][]

--- backend/typeDefs.ts
+++ backend/typeDefs.ts
@@ -19,6 +19,7 @@ export const typeDefs = gql`
 
    type Answer {
       key: Int!
+      location: Point!
       clue: String!
       answer: String!
    }
--- frontend/src/components/VerticalWord/CrosswordBoxContainer.tsx
+++ frontend/src/components/VerticalWord/CrosswordBoxContainer.tsx
@@ -2,7 +2,7 @@ import React, { FormEvent, useMemo } from 'react';
 
 import styled from 'styled-components';
 import CrosswordInputBox from './CrosswordInputBox';
-import { Crossword } from '../../generated/generated';
+import { Crossword, Point } from '../../generated/generated';
 import { useState } from 'react';
 import CrosswordBlankBox from './CrosswordBlankBox';
 import { useEffect } from 'react';
@@ -20,14 +20,15 @@ const CrosswordRow = styled.div`
 const BLANK_CHARACTER : string = '.' // determines which cells in the answer should be empty
 
 // Create a new empty string[][] filled with ''
-function createBlankGrid(dimension: number) : string[][] {
-  return Array(dimension).fill('').map(() =>(Array(dimension).fill('')));
+function createBlankGrid(dimension: number) : Point[][] {
+
+  return Array(dimension).fill(undefined as unknown as Point).map(() =>(Array(dimension).fill(undefined as unknown as Point)));
 }
 
 // Create a deep copy of a string[][]
-function deepCopy(array : string[][]) : string[][] {
+function deepCopy(array : Point[][]) : Point[][] {
   let dimension = array.length;
-  var newArray : string [][] = createBlankGrid(dimension);
+  var newArray : Point [][] = createBlankGrid(dimension);
       for(let i = 0; i < dimension; i++){
         for(let j = 0; j < dimension; j++){
             newArray[i][j] = array[i][j]
@@ -37,15 +38,15 @@ function deepCopy(array : string[][]) : string[][] {
 }
 
 // Compare two grids for equality
-function checkAnswer(currentArray : string[][], answer : string[][]) : boolean {
+function checkAnswer(currentArray : Point[][], answer : Point[][]) : boolean {
   if(currentArray.length !== answer.length){
     return false
   }
   let isEqual = true
   for(let i = 0; i < currentArray.length; i++){
     for(let j = 0; j < currentArray.length; j++){
-      if(currentArray[i][j] !== BLANK_CHARACTER && answer[i][j] !== BLANK_CHARACTER)
-        isEqual &&= currentArray[i][j].toUpperCase() === answer[i][j].toUpperCase()
+      if(currentArray[i][j].value !== BLANK_CHARACTER && answer[i][j].value !== BLANK_CHARACTER)
+        isEqual &&= currentArray[i][j].value.toUpperCase() === answer[i][j].value.toUpperCase()
     }
   }
   return isEqual
@@ -53,23 +54,34 @@ function checkAnswer(currentArray : string[][], answer : string[][]) : boolean {
 
 export type CrosswordBoxContainerProps = { crossword: Crossword };
 const CrosswordBoxContainer = ({ crossword }: CrosswordBoxContainerProps) => {  
-  // Initialize empty crossword grid 
-  console.log(crossword)
 
+  // Initialize empty crossword grid 
   const dimension : number = crossword.grid.dimension;
-  const [grid, setGrid] = useState<string[][]>(createBlankGrid(dimension))
-  console.log(crossword)
-  // Initialize the answer grid 
-  const answer = useMemo<string[][]>(() => {
-    var tempAnswer : string [][] = createBlankGrid(dimension);
+  
+  // Initialize the template grid 
+  const template = useMemo<Point[][]>(() => {
+    let tempAnswer : Point [][] = createBlankGrid(dimension);
     crossword.grid.points.forEach((point) => {
-       tempAnswer[point.y][point.x] = point.value
+       tempAnswer[point.y][point.x] = point
     })
     return tempAnswer
   }, [crossword, dimension] )
  
+  // Initialize the answer grid 
+  const answer = useMemo<Point[][]>(() => {
+    var tempAnswer : Point [][] = createBlankGrid(dimension);
+    crossword.grid.points.forEach((point) => {
+       tempAnswer[point.y][point.x] = point
+    })
+    return tempAnswer
+  }, [crossword, dimension] )
+
+  const [grid, setGrid] = useState<Point[][]>(template)
+
   // Check if the crossword is complete
   useEffect(() => {
+    console.log("grid", grid)
+    console.log("answer",answer)
     if(checkAnswer(grid, answer)){
       alert("success!")
     }
@@ -85,8 +97,8 @@ const CrosswordBoxContainer = ({ crossword }: CrosswordBoxContainerProps) => {
     event.currentTarget.textContent = event?.currentTarget?.textContent?.at(0) || "";
     let input : string  = event?.currentTarget?.textContent?.at(0) || "";
     setGrid(currentGrid => {
-      var newGrid : string [][] = deepCopy(currentGrid)
-      newGrid[rowIndex][columnIndex] = input ? input : currentGrid[rowIndex][columnIndex];
+      var newGrid : Point [][] = deepCopy(currentGrid)
+      newGrid[rowIndex][columnIndex].value = input ? input : currentGrid[rowIndex][columnIndex].value;
       return newGrid;
     });
     if (event.currentTarget.nextSibling) {
@@ -106,8 +118,8 @@ const CrosswordBoxContainer = ({ crossword }: CrosswordBoxContainerProps) => {
       (event.currentTarget as HTMLElement).textContent = '';
 
       setGrid(currentGrid => {
-        let newGrid : string [][] = deepCopy(currentGrid);
-        newGrid[rowIndex][columnIndex] =  "";
+        let newGrid : Point [][] = deepCopy(currentGrid);
+        newGrid[rowIndex][columnIndex].value =  "";
         return newGrid;
       });
       if(event.currentTarget?.previousSibling) {
@@ -124,11 +136,11 @@ const CrosswordBoxContainer = ({ crossword }: CrosswordBoxContainerProps) => {
 
   return (
     <CrosswordContainer>
-      {answer.map( (row : string[], i : number) => 
+      {answer.map( (row, i) => 
         <CrosswordRow key={i}>
-          {row.map( (letter : string, j : number) =>{ 
+          {row.map( (point, j) =>{ 
             let cellIndex = i*dimension + j
-            if(letter === BLANK_CHARACTER) {
+            if(point.value === BLANK_CHARACTER) {
               return <CrosswordBlankBox key={cellIndex}/>
             } else {
               return  <CrosswordInputBox
