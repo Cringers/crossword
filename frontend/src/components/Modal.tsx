8bc48d1d1b02d3d67d4aabf017368a8e106863e7
Ethan DeSon <ethandeson@gmail.com>
2022-05-07 20:29:31 -0700

    skeleton for win modal

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,8 +1,37 @@
+import { useState } from "react";
 import styled from "styled-components";
 
-export const Modal = styled.div`
+const ModalBackground = styled.div`
   position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  justify-content: center;
+  align-items: center;
+  display: flex;
   background: rgb(0, 0, 0, 0.7);
-  width: 100%;
-  height: 100%;
+  backdrop-filter: blur(2px);
 `;
+
+const ModalBox = styled.div`
+  background: whitesmoke;
+  padding: 1em;
+  border-radius: 1em;
+`
+
+export type ModalProps = { children: string };
+export const Modal = ({children,}: ModalProps) => {
+  let [showModal, setShowModal] = useState(true);
+
+  const closeModal = () => {
+    setShowModal(false)
+  }
+  const modal : JSX.Element | null = showModal ? 
+                  <ModalBackground>
+                    <ModalBox onClick={closeModal}> You win!</ModalBox> 
+                  </ModalBackground>
+                : null
+  
+  return <>{modal}</>
+}
