8983e7f9c6955f51e019ad36617e0c767fb345a7
Josh Ice <JoshIce@users.noreply.github.com>
2022-05-08 15:41:51 -0700

    Use Terraform to create/manage instances (#75)
    * terraform creates instance
    
    * execute cloud-config and add ssh keys
    
    * separate terraform files
    add new logic to action
    
    * Update terraformplan.yml
    
    * Update terraformplan.yml
    
    * separate terraform files
    add new logic to action
    
    * ignored .terraform generated folder
    
    * terraform apply now deletes old testing stack

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -3,11 +3,11 @@ locals {
 }
 
 provider "oci" {
-  tenancy_ocid = local.tenancy_ocid
-  user_ocid = "ocid1.user.oc1..aaaaaaaa7jqvwjbhlarubmfz2v6cdgjaztlbuhhalqnngsc2avt7e23dgcxa" 
+  tenancy_ocid     = local.tenancy_ocid
+  user_ocid        = "ocid1.user.oc1..aaaaaaaa7jqvwjbhlarubmfz2v6cdgjaztlbuhhalqnngsc2avt7e23dgcxa"
   private_key_path = "key.pem"
-  fingerprint = "8b:12:cd:4d:66:64:62:82:11:8c:2c:93:8c:c0:e4:81"
-  region = "us-phoenix-1"
+  fingerprint      = "8b:12:cd:4d:66:64:62:82:11:8c:2c:93:8c:c0:e4:81"
+  region           = "us-phoenix-1"
 }
 
 data "oci_core_images" "images" {
@@ -18,7 +18,11 @@ data "template_file" "cloud-config" {
   template = <<YAML
   #cloud-config
   runcmd:
-    - echo cringe > cringe.md
+    - yum install -y oracle-instantclient-release-el8
+	  - yum install -y oracle-instantclient-basic
+	  - sudo sh -c "echo /usr/lib/oracle/21/client64/lib > /etc/ld.so.conf.d/oracle-instantclient.conf"
+	  - sudo ldconfig
+    - curl https://get.volta.sh | bash
   YAML
 }
 
@@ -33,6 +37,6 @@ resource "oci_core_instance" "production" {
 
   metadata = {
     ssh_authorized_keys = "${file("authorized_keys")}"
-    user_data = "${base64encode(data.template_file.cloud-config.rendered)}"
+    user_data           = "${base64encode(data.template_file.cloud-config.rendered)}"
   }
 }
