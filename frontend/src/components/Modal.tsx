a3666888e18b101a68aa7b98ce2c8a4b4ee708f7
Ethan DeSon <ethandeson@gmail.com>
2022-05-08 17:27:44 -0700

    Adds modal only on win; escape closes modal

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useApolloClient } from "@apollo/client";
 import styled from "styled-components";
+import { IsAnsweredDocument, useIsAnsweredQuery } from "../generated/generated";
 
 const ModalBackground = styled.div`
   position: fixed;
@@ -12,24 +13,29 @@ const ModalBackground = styled.div`
   display: flex;
   background: rgb(0, 0, 0, 0.7);
   backdrop-filter: blur(2px);
+  z-index: 100;
 `;
 
 const ModalBox = styled.div`
   background: whitesmoke;
   padding: 1em;
   border-radius: 1em;
+  align-items: center;
 `
 
 export type ModalProps = { children: string };
 export const Modal = ({children,}: ModalProps) => {
-  const [showModal, setShowModal] = useState(true);
-
-  const closeModal = () => {
-    setShowModal(false)
+  const { data: answerData } = useIsAnsweredQuery();
+  const client = useApolloClient();
+  const onClose = (isAnswered: boolean ) => {
+      client.writeQuery({
+         query: IsAnsweredDocument,
+         data: { isAnswered }
+      })
   }
-  const modal : JSX.Element | null = showModal ? 
-                  <ModalBackground>
-                    <ModalBox onClick={closeModal}> You win!</ModalBox> 
+  const modal : JSX.Element | null = answerData?.isAnswered ? 
+                  <ModalBackground onClick={() => onClose(false)}>
+                    <ModalBox> You win!</ModalBox> 
                   </ModalBackground>
                 : null
   
